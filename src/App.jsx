ec69aa2992169d6636f0ab363b104aeb8a34b2af
hannah <hannahadora97@gmail.com>
2023-05-07 20:50:25 +0100

    update on faq page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
 import LandingPageLayout from './layout/LandingPageLayout'
 import Home from './routes/Home'
-// import AboutUs from './routes/AboutUs'
+import AboutUs from './routes/About'
 import './style/Home.css'
 import Market from './routes/Market'
 import Dashboard from './routes/Dashboard'
 import InvestMarket from './routes/InvestMarket'
 import BorrowMarket from './routes/BorrowMarket'
+import FAQs from './routes/Faq'
 
 const router = createBrowserRouter([
   {
@@ -34,14 +35,14 @@ const router = createBrowserRouter([
         path: '/BorrowMarket',
         element: <BorrowMarket />,
       },
-      // {
-      //   path: '/AboutUs',
-      //   element: <AboutUs />,
-      // },
-      // {
-      //   path: '/FAQs',
-      //   element: <FAQs />,
-      // },
+      {
+        path: '/AboutUs',
+        element: <AboutUs />,
+      },
+      {
+        path: '/FAQs',
+        element: <FAQs />,
+      },
     ],
   },
 ])
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import "../style/About.css"
+import believeIllustration from "../assets/svg/believe_img.svg"
+import challengesIllustration from "../assets/svg/challenges_img.svg"
+import solutionIllustration from "../assets/svg/solution_img.svg"
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section>
+      <header></header>
+      <main className="about_main-section">
+        <h3 >
+          Welcome to TITA, a decentralized micro-lending platform that connects
+          startups, investors, and entrepreneurs in an ecosystem that fosters
+          growth and success.
+        </h3>
+        <div>
+          <article className="about_grid">
+            <div>
+              <h5>WHAT WE BELIEVE</h5>
+              <p>
+                At TITA, we believe in providing equal opportunities to all
+                individuals, regardless of their background, gender, or
+                financial status. Our mission is to empower entrepreneurs and
+                small business owners access capital, resources, and mentorship
+                to help them achieve their goals.
+              </p>
+            </div>
+            <img src={believeIllustration} alt="" />
+          </article>
+          <article className="about_grid">
+            <img src={challengesIllustration} alt="" />
+            <div>
+              <h5>WHAT WE BELIEVE</h5>
+              <p>
+                At TITA, we believe in providing equal opportunities to all
+                individuals, regardless of their background, gender, or
+                financial status. Our mission is to empower entrepreneurs and
+                small business owners access capital, resources, and mentorship
+                to help them achieve their goals.
+              </p>
+            </div>
+          </article>
+          <article className="about_grid">
+            <div>
+              <h5>WHAT WE BELIEVE</h5>
+              <p>
+                At TITA, we believe in providing equal opportunities to all
+                individuals, regardless of their background, gender, or
+                financial status. Our mission is to empower entrepreneurs and
+                small business owners access capital, resources, and mentorship
+                to help them achieve their goals.
+              </p>
+            </div>
+            <img src={solutionIllustration} alt="" />
+          </article>
+        </div>
+      </main>
+    </section>
+  );
+};
 
-export default About
+export default About;
