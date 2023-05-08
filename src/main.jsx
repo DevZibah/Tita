af4be1a97c457e8988577d0b9f4e1d2fe0f77805
hannah <hannahadora97@gmail.com>
2023-05-08 12:48:23 +0100

    celo provider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import './index.css'
 import NavbarContextProvider from './contexts/NavbarContext'
+import { CeloProvider } from "@celo/react-celo";
+import "@celo/react-celo/lib/styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <CeloProvider>
     <NavbarContextProvider>
       <App />
     </NavbarContextProvider>
+    </CeloProvider>
   </React.StrictMode>
 )
