dea73710fb3c6d6741c448c1bf9b0008291b9667
hannah <hannahadora97@gmail.com>
2023-05-10 14:26:27 +0100

    update

--- src/components/TheTeam.tsx
+++ src/components/TheTeam.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { teamMembers, ITeamMember } from "../utils/team-members";
-import "../style/TheTeam.css"
+import "../style/TheTeam.css";
 
 const TheTeam = () => {
   return (
     <section className="d_team_wrapper">
-      <h3 className="stellar_text">THE STELLAR TEAM</h3>
+      <h3 className="stellar_text">MEET THE TEAM</h3>
       <ul className="d_team_grid">
         {teamMembers.map((member: ITeamMember, idx) => (
           <li key={idx}>
             <img className="member_photo" src={member.photo} alt="photo" />
-            <p className="member_name">{member.name}</p>
-            <ul>
-              {member.positions.map((position) => (
-                <li className="member_position">{position}</li>
-              ))}
-            </ul>
+            <div>
+              <p className="member_name">{member.name}</p>
+              <ul>
+                {member.positions.map((position) => (
+                  <li className="member_position">{position}</li>
+                ))}
+              </ul>
+            </div>
           </li>
         ))}
       </ul>
